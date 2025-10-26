e761fb7aa0a7250483bfa058eeb9120e594fff40
hvdangio <hvdang.io@gmail.com>
2025-10-26 11:41:46 +0700

    TTS KEY*: ABC-123 FUNC CHANGES*: First mini bot_101 for funn TECH CHANGES*: model_name = "HuggingFaceTB/SmolLM2-135M" for FAST response and test param(s) OK TEST: response in a few seconds OK, test param(s) OK, TODO: need to improve the quality/reasoning

--- src/main.py
+++ src/main.py
@@ -1,15 +1,33 @@
+import torch
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
 
-##1. LOAD AI-model:
-from transformers import pipeline
-chat = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.1")
+##1. LOAD AI-model: ---------------
+#model_name = "mistralai/Mistral-7B-Instruct-v0.1"
+#model_name = "deepseek-ai/DeepSeek-V2-Lite"
+model_name = "HuggingFaceTB/SmolLM2-135M"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+chat = pipeline(
+  "text-generation",
+  model=model,
+  tokenizer=tokenizer,
+  device_map="cpu",  #no GPU now
+  #torch_dtype=torch.float16  #try for performance
+)
 print(chat("User: Xin chào! Bot:", max_length=100))
 
-##2. LOAD UI:
-import gradio as gr
 
+##2. Define chatbot: ---------------
 def chatbot(msg, history):
-    response = chat(msg)[0]['generated_text']
-    history.append((msg, response)) #funn check
-    return response
+  full_prompt = msg #no_history
+  output = chat(full_prompt, max_new_tokens=50, do_sample=True
+                , temperature=0.7, max_length=200
+                )[0]["generated_text"]
+  # history = history + [(msg, output)]
+  return output
+
+
+##3. LOAD UI: ---------------
+import gradio as gr
 gr.ChatInterface(chatbot).launch()
