22e7a5c19a0dcd57f23b0ad92e47057b324a1bdf
hvdangio <hvdang.io@gmail.com>
2025-10-26 17:11:08 +0700

    TTS KEY*: ABC-123 FUNC CHANGES*: First mini bot_101 for funn TECH CHANGES*: With basic_reasoning within ~ 20 seconds response   model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0" replace model_name = "HuggingFaceTB/SmolLM2-135M" TEST: Response OK for question like "How to play Chess", OR: "how to learn Python, Pandas"   But for question like "Cac mon an ngon o VietNam thi van hoi chuoi."   FORMAT respone on gradio/UI still very_basic, no mark_down etc...

--- src/main.py
+++ src/main.py
@@ -9,31 +9,34 @@ def log_response(input_msg, output_text):
     f.write(f"USER: {input_msg}\n")
     f.write(f"BOT : {output_text}\n")
 
-def clean_bot_reply(output_text):
-  parts = output_text.split("Assistant:")
-  if len(parts) > 1:
-    return parts[-1].strip()
-  return output_text.strip()
-
 
 ##1. LOAD AI-model: ---------------
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 #model_name = "mistralai/Mistral-7B-Instruct-v0.1"
 #model_name = "deepseek-ai/DeepSeek-V2-Lite"
-model_name = "HuggingFaceTB/SmolLM2-135M"
+#model_name = "HuggingFaceTB/SmolLM2-135M"
+model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 chat = pipeline(
   "text-generation",
   model=model,
   tokenizer=tokenizer,
-  device_map="cpu",  #no GPU now
+  device_map="auto",
   #torch_dtype=torch.float16  #try for performance
 )
 print(chat("User: Xin chào! Bot:", max_length=100))
 
 
 ##2. Define chatbot: ---------------
+def extract_first_new_assistant_reply(generated_text):
+  if "NEW: Assistant:" in generated_text:
+    reply = generated_text.split("NEW: Assistant:", 1)[1]
+    reply = reply.split("NEW: User:", 1)[0]
+    return reply.strip()
+  return generated_text.strip()
+
+
 def chatbot(msg, history):
   # Step 2.1: System prompt (role = system)
   system_prompt = {
@@ -51,14 +54,13 @@ def chatbot(msg, history):
     if m["role"] in ("user", "assistant"):
       messages.append(m)
 
-  # New message from user
-  messages.append({"role": "user", "content": msg})
-
   # Step 2.3: prompt_text
   prompt_text = ""
   for m in messages:
-    prompt_text += f"{m['role'].capitalize()}: {m['content']}\n"
-  prompt_text += "Assistant:"
+    prompt_text += f"CONTEXT: {m['role'].capitalize()}: {m['content']}\n"
+  # New message from user
+  prompt_text += f"NEW: {"user".capitalize()}: {msg}\n"
+  prompt_text += "NEW: Assistant:"
 
   output = chat(
     prompt_text,
@@ -69,7 +71,8 @@ def chatbot(msg, history):
   log_response(msg, output)
 
   # Step 3.4: Return
-  bot_reply = clean_bot_reply(output)
+  generated_text = output[len(prompt_text) - len("NEW: Assistant:"):]
+  bot_reply = extract_first_new_assistant_reply(generated_text)
   return {"role": "assistant", "content": bot_reply}
 
 
